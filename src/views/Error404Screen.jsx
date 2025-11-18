c1ffdc8d04a50c21ad4f08356e295212f84506f6
Pablo Marino <pmarino2013@gmail.com>
2025-11-17 21:33:39 -0300

    Primer commit, creación de rutas básicas

---
+++ src/views/Error404Screen.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Error404Screen = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>Not found 😭</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Error404Screen;
